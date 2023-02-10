5ddafc5783577f987a312d77086163fdeda888e1
DELL <DELL@DESKTOP-D948P61>
2023-02-10 14:27:53 +0530

    First commit

---
+++ dayfour/src/org/tnsif/singlelevelinheritance/SingleInheritanceDemo.java
@@ -0,0 +1,13 @@
+package org.tnsif.singlelevelinheritance;
+
+public class SingleInheritanceDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s=new Student();
+		Student s1=new Student("Pratik","345612349873","Pune",98984567L,101,"Pratik Gosavi");
+		System.out.println(s1);
+
+	}
+
+}
